f0f036332d1352fe2d34d588763eebb79676bffe
lumap-svg <lutanywapeter@gmail.com>
2024-05-18 03:10:40 -0700

    removed unused import on index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Testing from './testing';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
